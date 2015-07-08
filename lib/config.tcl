94ac11d22e950b80df10bd42f449356c31011fa6
BeF <bef@pentaphase.de>
2015-07-08 15:50:15 +0200

    open ini file read-only

--- lib/config.tcl
+++ lib/config.tcl
@@ -3,11 +3,11 @@ package require inifile
 proc read_config {etc} {
 	global cfg
 	
-	set ini [::ini::open "$etc/email.ini"]
+	set ini [::ini::open "$etc/email.ini" r]
 	set cfg(email) [::ini::get $ini email]
 	::ini::close $ini
 	
-	set ini [::ini::open "$etc/groups.ini"]
+	set ini [::ini::open "$etc/groups.ini" r]
 	set cfg(groups) {}
 	foreach sec [::ini::sections $ini] {
 		 lappend cfg(groups) $sec [::ini::get $ini $sec]
