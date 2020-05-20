4ecd3f8b3bd39f8e2f645fee0fb7ff404afcf6cf
BeF <bef@pentaphase.de>
2020-05-20 14:46:23 +0200

    added 'mustchange' option to send email only if data changed

--- statusbot.tcl
+++ statusbot.tcl
@@ -8,6 +8,7 @@
 
 package require Tcl 8.5
 package require cmdline
+
 if {[info commands try] eq ""} {
 	package require try
 }
@@ -20,6 +21,7 @@ set options {
 	{subject.arg "hello" "email subject"}
 	{stdin "read email body from stdin"}
 	{url.arg "" "download email body from URL"}
+	{mustchange.arg "" "checksum file. email will only be sent if data changed"}
 }
 
 set here [file dirname [info script]]
@@ -34,13 +36,20 @@ try {
 }
 
 
-##
+## prepare tclgpg
+if {![info exists ::env(GPG_AGENT_INFO)]} {
+	# puts [exec gpg-connect-agent "GETINFO socket_name" "GETINFO pid" /bye]
+	set ::env(GPG_AGENT_INFO) "foo"
+}
 
+## load libs
 lappend auto_path $params(libdir)
 foreach i {config get_http gpg_encrypt email} { source "${params(libdir)}/$i.tcl" }
 
+## read config
 read_config $params(inidir)
 
+## execute cmd
 switch $params(cmd) {
 	send {
 		if {!$params(stdin) && $params(url) eq ""} { puts "no body?"; exit 1 }
@@ -51,6 +60,18 @@ switch $params(cmd) {
 			set data [read stdin]
 		}
 		if {$data eq ""} { puts "empty data."; exit 2 }
+
+		if {$params(mustchange) ne ""} {
+			package require fileutil
+			package require md5
+
+			set md5_new [::md5::md5 -hex $data]
+			if {[file exists $params(mustchange)]} {
+				set md5_prev [::fileutil::cat $params(mustchange)]
+				if {$md5_new eq $md5_prev} { exit 0 }
+			}
+			::fileutil::writeFile $params(mustchange) $md5_new
+		}
 
 		send_smtp_encrypted -group $params(group) -subject $params(subject) -body $data
 	}
